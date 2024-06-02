8dd1ee821f8311e322d8fee2fe1a2da2ee8d3791
Lorenzo Carlassara <lorenzo.carlassara@mail.polimi.it>
2024-06-02 15:53:14 +0200

    update b2m

--- AL_VSVM_cologne_b2m_shape new.R
+++ AL_VSVM_cologne_b2m_shape new.R
@@ -23,14 +23,13 @@ train  = TRUE         # if TRUE, train the models otherwise load them from dir
 save_models = FALSE   # if TRUE, save the models into dir after training
 if(binary){
   model_class="binary"
-  sampleSizePor = c(2,3,5,10,17,27,42,67,83,100) # vector with % of max  # c(40,25,16,12,10,8,6,4,3,2,1) 
+  sampleSizePor = c(3,5,10,17,27,40,64,80) # vector with % of max  # c(40,25,16,12,10,8,6,4,3,2,1) 
   b = 10   # Size of balanced_unlabeled_samples in each class
 }else{
   model_class="multiclass"
-  sampleSizePor = c(5,10,17,27,42,67,83,100) # Class sample size: round(250/6) label per class i.e. 42
+  sampleSizePor = c(5,10,17,27,40,64,80,100) # Class sample size: round(250/6) label per class i.e. 42
   b = 20   # Size of balanced_unlabeled_samples in each class
 } 
-
 path = '/home/rsrg9/Documents/tunc_oz/apply_model/'
 model_path = "/home/rsrg9/Documents/GitHub/active-learning-virtual-SVM/"
 if(!dir.exists(path)){path = "D:/tunc_oz/apply_model/"
