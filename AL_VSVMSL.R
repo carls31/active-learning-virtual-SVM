5216dfc5a0a90a9f807ccc66ec83a70942346bc9
Lorenzo Carlassara <lorenzo.carlassara@mail.polimi.it>
2024-07-10 18:55:00 +0200

    update AL_VSVM+SL

--- AL_VSVMSL.R
+++ AL_VSVMSL.R
@@ -16,7 +16,7 @@ bound = c(0.3, 0.6, 0.9)           # radius around SV - threshold    # c(0.3, 0.
 boundMargin = c(1.5, 1, 0.5)       # distance from hyperplane - threshold   # c(1.5, 1, 0.5) # c(1.5, 1)
 sampleSizePor = c(5,10,20,32,46,62,80,100) # Class sample size: round(250/6) label per class i.e. 42 # c(100,80,62,46,32,20,10,5)
 
-resampledSize = c(3*b)    # total number of relabeled samples # b, 2*b, 3*b, 6*b
+resampledSize = c(2.5*b)    # total number of relabeled samples # b, 2*b, 3*b, 6*b
 newSizes = c(0.5*b) # = resampledSize[rS]       # number of samples picked per iteration # 4, 5, 10, 20, resampledSize
 # classSize = c(100*b) #1200 # number of samples per class # 25, 50, 75, 100, 150, 300, 580 for multiclass #  min(100*b,as.numeric(min(table(trainDataCurRemaining$REF)))/3)
 clusterSizes = c(0.6*b,6*b) #60*b # number of clusters used to pick samples from different groups # 40, 60, 80, 100, 120, 300
@@ -641,7 +641,7 @@ classificationProblem = function(generalDataPool){
 }
 ########################################  Preprocessing  ########################################
 for (model_prob in model_probs) {
-  if (model_prob=="binary") { sampleSizePor = c(2,5,10,20,35,53,75,100) # c(100,75,53,35,20,10,5,2)
+  if (model_prob=="binary") { # sampleSizePor = c(2,5,10,20,35,53,75,100) # c(100,75,53,35,20,10,5,2)
   # bound = c(0.3, 0.9)          
   # boundMargin = c(1.5, 1, 0.5)
   # resampledSize = c(2*b)
