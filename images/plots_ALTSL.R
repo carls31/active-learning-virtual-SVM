abc67c5fea042379a62588eb3efe17cef378cdee
lorenzocarlassara <lorenzo.carlassara98@gmail.com>
2024-08-22 19:12:44 +0200

    update img result fail

--- images/plots_ALTSL.R
+++ images/plots_ALTSL.R
@@ -20,6 +20,7 @@ file_name_acc = "20240817_0730_hagadera_multiclass_scale_acc_ALTSLf_20Unl_10nR_9
 file_name_acc = "20240819_1244_cologne_multiclass_scale_acc_ALTSLf_20Unl_10nR_9SizePor"
 file_name_acc = "20240819_1904_hagadera_binary_scale_acc_ALTSLf_20Unl_8nR_16SizePor"
 file_name_acc = "20240821_0601_cologne_binary_scale_acc_ALTSLf_20Unl_8nR_16SizePor"
+file_name_acc = "20240821_2337_cologne_binary_scale_acc_ALTSLv3_20Unl_8nR_16SizePor"
 
 
 
@@ -31,6 +32,7 @@ file_name_kappa = "20240817_0730_hagadera_multiclass_scale_Kappa_ALTSLf_20Unl_10
 file_name_kappa = "20240819_1244_cologne_multiclass_scale_Kappa_ALTSLf_20Unl_10nR_9SizePor"
 file_name_kappa = "20240819_1904_hagadera_binary_scale_Kappa_ALTSLf_20Unl_8nR_16SizePor"
 file_name_kappa = "20240821_0601_cologne_binary_scale_Kappa_ALTSLf_20Unl_8nR_16SizePor"
+file_name_kappa = "20240821_2337_cologne_binary_scale_Kappa_ALTSLv3_20Unl_8nR_16SizePor"
 
 
 
@@ -100,25 +102,25 @@ load(paste0(file_name_kappa,".RData"))
 # file_name_acc = "20240711_cologne_multiclass_shape_acc_20Unl_5nR_8SizePor"
 # file_name_kappa = "20240711_cologne_multiclass_shape_Kappa_20Unl_5nR_8SizePor"
 # # **********************************************************************************
-# AccuracySVM=AccuracySVM[1:4,]
-# AccuracySVM_SL_Un=AccuracySVM_SL_Un[1:4,]
-# AccuracyVSVM_SL=AccuracyVSVM_SL[1:4,]
-# AccuracyVSVM_SL_Un=AccuracyVSVM_SL_Un[1:4,]
-# AccuracyVSVM_SL_vUn=AccuracyVSVM_SL_vUn[1:4,]
-# AccuracyVSVM_SL_Un_random_it=AccuracyVSVM_SL_Un_random_it[1:4,]
-# AccuracyVSVM_SL_Un_it=AccuracyVSVM_SL_Un_it[1:4,]
-# AccuracyVSVM_SL_Un_itSL=AccuracyVSVM_SL_Un_itSL[1:4,]
-# AccuracyVSVM_SL_Un_itTSL=AccuracyVSVM_SL_Un_itTSL[1:4,]
+# AccuracySVM=AccuracySVM[1:5,]
+# AccuracySVM_SL_Un=AccuracySVM_SL_Un[1:5,]
+# AccuracyVSVM_SL=AccuracyVSVM_SL[1:5,]
+# AccuracyVSVM_SL_Un=AccuracyVSVM_SL_Un[1:5,]
+# AccuracyVSVM_SL_vUn=AccuracyVSVM_SL_vUn[1:5,]
+# AccuracyVSVM_SL_Un_random_it=AccuracyVSVM_SL_Un_random_it[1:5,]
+# AccuracyVSVM_SL_Un_it=AccuracyVSVM_SL_Un_it[1:5,]
+# AccuracyVSVM_SL_Un_itSL=AccuracyVSVM_SL_Un_itSL[1:5,]
+# AccuracyVSVM_SL_Un_itTSL=AccuracyVSVM_SL_Un_itTSL[1:5,]
 # 
-# KappaSVM=KappaSVM[1:4,]
-# KappaSVM_SL_Un=KappaSVM_SL_Un[1:4,]
-# KappaVSVM_SL=KappaVSVM_SL[1:4,]
-# KappaVSVM_SL_Un=KappaVSVM_SL_Un[1:4,]
-# KappaVSVM_SL_vUn=KappaVSVM_SL_vUn[1:4,]
-# KappaVSVM_SL_Un_random_it=KappaVSVM_SL_Un_random_it[1:4,]
-# KappaVSVM_SL_Un_it=KappaVSVM_SL_Un_it[1:4,]
-# KappaVSVM_SL_Un_itSL=KappaVSVM_SL_Un_itSL[1:4,]
-# KappaVSVM_SL_Un_itTSL=KappaVSVM_SL_Un_itTSL[1:4,]
+# KappaSVM=KappaSVM[1:5,]
+# KappaSVM_SL_Un=KappaSVM_SL_Un[1:5,]
+# KappaVSVM_SL=KappaVSVM_SL[1:5,]
+# KappaVSVM_SL_Un=KappaVSVM_SL_Un[1:5,]
+# KappaVSVM_SL_vUn=KappaVSVM_SL_vUn[1:5,]
+# KappaVSVM_SL_Un_random_it=KappaVSVM_SL_Un_random_it[1:5,]
+# KappaVSVM_SL_Un_it=KappaVSVM_SL_Un_it[1:5,]
+# KappaVSVM_SL_Un_itSL=KappaVSVM_SL_Un_itSL[1:5,]
+# KappaVSVM_SL_Un_itTSL=KappaVSVM_SL_Un_itTSL[1:5,]
 # # **********************************************************************************
 # 
 # save(AccuracySVM,
