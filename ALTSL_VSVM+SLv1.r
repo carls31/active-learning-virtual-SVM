e684709433dd9b6b0ac7be949c548a426544f007
Lorenzo Carlassara <lorenzo.carlassara@mail.polimi.it>
2024-09-24 09:57:03 +0200

    update result col b scale v1

--- ALTSL_VSVM+SLv1.r
+++ ALTSL_VSVM+SLv1.r
@@ -12,8 +12,8 @@ library(Rtsne)      # t-distributed stochastic neighbour embedding
 
 nR = 10                  # number of realizations
 cities = c("cologne")     # cologne or hagadera location
-invariances = c("scale")   # scale or shape invariance
-model_probs = c("binary")  # multiclass or binary problem
+invariances = c("shape")   # scale or shape invariance
+model_probs = c("multiclass")  # multiclass or binary problem
 
 b = c(20)                     # size of balanced_unlabeled_samples per class
 bound = c(0.3, 0.6)      # radius around SV - threshold          
